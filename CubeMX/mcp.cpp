c01ed1c22826127325b83b3129de31b21c693beb
Tinic Uro <tinicuro@gmail.com>
2025-04-16 01:28:31 -0700

    .

--- CubeMX/mcp.cpp
+++ CubeMX/mcp.cpp
@@ -12,4 +12,47 @@ MCP &MCP::instance() {
 }
 
 void MCP::Slice() {
+    static int timerRefill = 0;
+    static bool timerRefillActive = false;
+
+    static int timerShutoff = 0;
+    static bool timerShutoffActive = false;
+
+    bool solenoid0 = false;
+    bool solenoid1 = false;
+
+    if (PSI1() < 75.0f) {
+        timerRefillActive = true;
+    }
+
+    if (PSI1() >= 100.0f) {
+        timerShutoffActive = true;
+    }
+
+    if (timerRefill >= 10) {
+        solenoid0 = true;
+        if (PSI0() > 75.0f) {
+            solenoid1 = true;
+        }
+    }
+
+    if (timerShutoff >= 5) {
+        solenoid0 = false;
+        solenoid1 = false;
+        timerRefillActive = false;
+        timerRefill = 0;
+        timerShutoffActive = false;
+        timerShutoff = 0;
+    }
+
+    if (timerRefillActive) {
+        timerRefill++;
+    }
+
+    if (timerShutoffActive) {
+        timerShutoff++;
+    }
+
+    SetSolenoid0(solenoid0);
+    SetSolenoid1(solenoid1);
 }
